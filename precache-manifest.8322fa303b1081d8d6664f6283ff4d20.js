4ccadd35276566bc4527033bf009a2b681739d92
xakim <xakim@pop-os.localdomain>
2020-09-29 16:47:26 +0600

    Updates

---
+++ precache-manifest.8322fa303b1081d8d6664f6283ff4d20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd041fdabf7d1fd93f96f0cb268b400",
+    "url": "/news-feed/index.html"
+  },
+  {
+    "revision": "9052dd4200f87f702069",
+    "url": "/news-feed/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a1e83c49618e11c3d171",
+    "url": "/news-feed/static/js/2.0b967129.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/news-feed/static/js/2.0b967129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9052dd4200f87f702069",
+    "url": "/news-feed/static/js/main.02bc6e66.chunk.js"
+  },
+  {
+    "revision": "37473beedcc8996641a7",
+    "url": "/news-feed/static/js/runtime-main.84695215.js"
+  }
+]);
